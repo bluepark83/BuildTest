97f36bd835e1da645046c412e8facba1002add39
bluepark83 <https://github.com/bluepark83/ProjectK.git>
2021-09-13 23:27:06 +0900

    feat : 빌드 시스템

---
+++ Assets/Editor/ScriptBatch.cs
@@ -0,0 +1,17 @@
+using UnityEditor;
+
+public class ScriptBatch 
+{
+    [MenuItem("MyTools/Windows Build With Postprocess")]
+    public static void BuildGame ()
+    {
+        // Get filename.
+        string path = EditorUtility.SaveFolderPanel("Choose Location of Built Game", "", "");
+        string[] levels = new string[] {"Assets/Scenes/Scene1.unity"};
+
+        // Build player.
+        BuildPipeline.BuildPlayer(levels, path + "/BuiltGame.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
+
+        UnityEngine.Debug.Log("Complete Build");
+    }
+}
