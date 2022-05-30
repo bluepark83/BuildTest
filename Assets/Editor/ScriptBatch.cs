a775968ba385a0cba9beb71fcb508251cc6de63d
bluepark83 <bluepark83@naver.com>
2022-05-30 22:45:10 +0900

    update
    파일정리

--- Assets/Editor/ScriptBatch.cs
+++ Assets/Editor/ScriptBatch.cs
@@ -1,17 +1,39 @@
 using UnityEditor;
+using UnityEngine;
 
-public class ScriptBatch 
+public class ScriptBatch
 {
     [MenuItem("MyTools/Windows Build With Postprocess")]
-    public static void BuildGame ()
+    public static void BuildGame()
     {
-        // Get filename.
-        string path = EditorUtility.SaveFolderPanel("Choose Location of Built Game", "", "");
-        string[] levels = new string[] {"Assets/Scenes/Scene1.unity"};
-
-        // Build player.
-        BuildPipeline.BuildPlayer(levels, "./Build/BuiltGame.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
+        var commandLineArgs = System.Environment.GetCommandLineArgs();
+        foreach (var argument in commandLineArgs)
+        {
+            Debug.Log(argument);   
+        }
+        
+        // // Get filename.
+        // string path = EditorUtility.SaveFolderPanel("Choose Location of Built Game", "", "");
+        // string[] levels = new string[] { "Assets/Scenes/Scene1.unity" };
+        //
+        // // Build player.
+        // BuildPipeline.BuildPlayer(levels, "./Build/BuiltGame.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
 
         UnityEngine.Debug.Log("Complete Build");
+    }
+
+
+    static string GetArg(string name)
+    {
+        var args = System.Environment.GetCommandLineArgs();
+        for (int i = 0; i < args.Length; i++)
+        {
+            if (args[i] == name && args.Length > i + 1)
+            {
+                return args[i + 1];
+            }
+        }
+
+        return default;
     }
 }
